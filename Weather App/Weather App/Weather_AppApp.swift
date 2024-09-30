56284f02f7d15922a9382631b644334ef91d1b16
João Alves <joaoalves@Macmini.local>
2024-09-30 17:04:22 -0300

    Projeto criado.

---
+++ Weather App/Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by João Alves on 30/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
